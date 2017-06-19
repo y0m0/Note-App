f99dc45f4a6ede8d70da765c59fac294d4195a22
elenamorton <elena.uleia@gmail.com>
2017-06-19 17:17:02 +0100

    Added a node to  list and unit tested.

--- model/list.js
+++ model/list.js
@@ -1,7 +1,13 @@
+'use strict';
+
 (function(exports) {
   function List() {
     this.notes = [];
-  };
+  }
 
+  List.prototype.addNote = function(note) {
+    this.notes.push(note);
+  }
+  
   exports.List = List;
 })(this);
--- test/listTest.js
+++ test/listTest.js
@@ -1,10 +1,30 @@
+'use strict';
+
 (function(exports) {
+  
   function testListNotesDefaultsToEmptyArray() {
     var list = new List();
     if (!(Array.isArray(list.notes)) || list.notes.length !== 0) {
       throw new Error("Notes is not an empty array");
     }
-  };
+  }
+  
+    function testListMethodAddNote() {
+    
+    function NoteSpy() {
+    }
+      
+    var list = new List();
+    var noteSpy = new NoteSpy();
+      
+    list.addNote(noteSpy);
+      
+    if (list.notes[0] !== noteSpy) {
+      throw new Error("Note is not added to the list");
+    }
+  }
 
   testListNotesDefaultsToEmptyArray();
+  testListMethodAddNote();
+  
 })(this);
