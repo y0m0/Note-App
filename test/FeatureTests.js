75b772d3b14566b43ec64b5d7d78790a7eeb2218
elenamorton <elena.uleia@gmail.com>
2017-06-20 11:44:29 +0100

    Added feature test for creating a note.

--- test/FeatureTests.js
+++ test/FeatureTests.js
@@ -11,10 +11,42 @@
     else {
       console.log(arguments.callee.name + " test passed!")
     }
-    
   }
   
+  function featureTestCreateNote() {
+    var note = document.getElementById("note-body");
+    var button = document.getElementById("create-note");
+    var li = document.getElementsByTagName("li");
+
+    note.innerHTML = "Some content for this note.";
+    
+    // Simulate clicking on the specified element.
+    triggerEvent( button, 'click' );
+    
+    if ( li.length !== 1) {
+      throw new Error( "Note is not on the page" );
+    }
+    else {
+      console.log(arguments.callee.name + " test passed!")
+    }
+    
+    /**
+    * Trigger the specified event on the specified element.
+    * @param  {Object} elem  the target element.
+    * @param  {String} event the type of the event (e.g. 'click').
+    */
+    function triggerEvent( elem, event ) {
+      var clickEvent = new Event( event ); // Create the event.
+      elem.dispatchEvent( clickEvent );    // Dispatch the event.
+    }
+  }
+  
+  
+  
+  
+  // Run the feature tests
   featureTestDisplayNoteHeader("Notes");
+  featureTestCreateNote();
   
 })(this);
  
