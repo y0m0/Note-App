a27b3c5176fae17ed38b10b71461e18f2ed217d6
y0m0 <lixi.daniele@gmail.com>
2017-06-20 16:48:14 +0100

    Adds test for long notes being displayed in list and shortened to only 20 chars

--- test/FeatureTests.js
+++ test/FeatureTests.js
@@ -1,37 +1,37 @@
 //'use strict';
 
 (function(exports) {
-  
+
   function featureTestDisplayNoteHeader(h1Name) {
     var h1 = document.getElementById("header").innerHTML;
-    
+
     if (h1 !== h1Name) {
       throw new Error( h1Name + " is not on the page" );
     }
     else {
-      console.log(arguments.callee.name + " test passed!")
+      console.log(arguments.callee.name + " test passed!");
     }
   }
-  
+
   function featureTestCreateNote() {
     var note = document.getElementById("note-body");
     var button = document.getElementById("createButton");
     var ulChildren;
-    
+
     note.value = "Some content for this note.";
-    
+
     // Simulate clicking on the specified element.
     triggerEvent( button, 'click' );
     ulChildren = document.getElementById("noteList").children;
-    
+
     if ( ulChildren.length !== 1) {
       throw new Error( "Note is not on the page" );
     }
     else {
-      console.log(arguments.callee.name + " test passed!")
+      console.log(arguments.callee.name + " test passed!");
     }
   }
-  
+
   function featureTestCreateMultipleNotes() {
     var note = document.getElementById("note-body");
     var button = document.getElementById("createButton");
@@ -43,18 +43,36 @@
     triggerEvent( button, 'click' );
     note.value = "One more note.";
     triggerEvent( button, 'click' );
-    
+
     ulChildren = document.getElementById("noteList").children;
-    
+
     if ( ulChildren.length !== 4) {
       throw new Error( "Wrong number of notes in the list" );
     }
     else {
-      console.log(arguments.callee.name + " test passed!")
+      console.log(arguments.callee.name + " test passed!");
     }
   }
-  
-  
+
+  function featureTestVeryLongNote() {
+    var note = document.getElementById("note-body");
+    var button = document.getElementById("createButton");
+    var ulChildren;
+
+    note.value = ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
+
+    // Simulate clicking on the specified element.
+    triggerEvent( button, 'click' );
+    ulChildren = document.getElementById("noteList").children;
+    if ( ulChildren[ulChildren.length-1].innerText !== "Lorem ipsum dolor si...") {
+      throw new Error( "Note is longer the 20 chars" );
+    }
+    else {
+      console.log(arguments.callee.name + " test passed!");
+    }
+  }
+
+
   /*
   * Trigger the specified event on the specified element.
   * @param  {Object} elem  the target element.    
@@ -64,13 +82,13 @@
     var clickEvent = new Event( event );
     elem.dispatchEvent( clickEvent );    // Dispatch the event.   
   }
-  
+
   // Run the feature tests
   window.onload = function() {
-    console.log("inside window test load");
     featureTestDisplayNoteHeader("Notes");
     featureTestCreateNote();
     featureTestCreateMultipleNotes();
+    featureTestVeryLongNote();
   };
 })(this);
- 
+
