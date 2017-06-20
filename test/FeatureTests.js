3111dff7f8d2f1da6536db225bb76da2204b7ee3
y0m0 <lixi.daniele@gmail.com>
2017-06-20 17:47:44 +0100

    Adds test for displaying full message on lightbox popup - refactors tests - adds helper function to clean notes after each test

--- test/FeatureTests.js
+++ test/FeatureTests.js
@@ -29,6 +29,7 @@
     }
     else {
       console.log(arguments.callee.name + " test passed!");
+      cleanList();
     }
   }
 
@@ -37,42 +38,64 @@
     var button = document.getElementById("createButton");
     var ulChildren;
 
-    note.value = "First content for this note.";
-    triggerEvent( button, 'click' );
-    note.value = "Another text.";
-    triggerEvent( button, 'click' );
-    note.value = "One more note.";
-    triggerEvent( button, 'click' );
+    for(var i = 0; i < 3; i++) {
+      note.value = "note number " + i ;
+      triggerEvent( button, 'click' );
+    }
 
     ulChildren = document.getElementById("noteList").children;
 
-    if ( ulChildren.length !== 4) {
+    if ( ulChildren.length !== 3) {
       throw new Error( "Wrong number of notes in the list" );
     }
     else {
       console.log(arguments.callee.name + " test passed!");
+      cleanList();
     }
   }
 
   function featureTestVeryLongNote() {
+    var longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
     var note = document.getElementById("note-body");
     var button = document.getElementById("createButton");
     var ulChildren;
 
-    note.value = ("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.");
+    note.value = longText;
 
-    // Simulate clicking on the specified element.
     triggerEvent( button, 'click' );
     ulChildren = document.getElementById("noteList").children;
+
     if ( ulChildren[ulChildren.length-1].innerText !== "Lorem ipsum dolor si...") {
       throw new Error( "Note is longer the 20 chars" );
     }
     else {
       console.log(arguments.callee.name + " test passed!");
+      cleanList();
     }
   }
 
 
+  function featureTestDisplayWholeNote() {
+    var longText = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
+    var note = document.getElementById("note-body");
+    var button = document.getElementById("createButton");
+
+    note.value = longText;
+
+    triggerEvent( button, 'click' );
+    var liPreview = document.getElementById("noteList").children[0];
+
+    triggerEvent( liPreview, 'click' );
+
+    if ( document.getElementById('note-view').innerText !== longText) {
+      throw new Error( "Note is longer the 20 chars" );
+    }
+    else {
+      console.log(arguments.callee.name + " test passed!");
+      cleanList();
+      triggerEvent(document.getElementById('outer'), 'click');
+    }
+  }
   /*
   * Trigger the specified event on the specified element.
   * @param  {Object} elem  the target element.    
@@ -83,12 +106,17 @@
     elem.dispatchEvent( clickEvent );    // Dispatch the event.   
   }
 
+  function cleanList() {
+    document.getElementById('noteList').innerHTML = '';
+  }
+
   // Run the feature tests
   window.onload = function() {
     featureTestDisplayNoteHeader("Notes");
     featureTestCreateNote();
     featureTestCreateMultipleNotes();
     featureTestVeryLongNote();
+    featureTestDisplayWholeNote();
   };
 })(this);
 
